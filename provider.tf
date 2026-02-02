e165581a39280365a5236f4779a91daa9f94d768
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2026-02-02 16:06:25 +0530

    Updated

--- provider.tf
+++ provider.tf
@@ -2,18 +2,3 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_iam_user" "Test-1" {
-  name = "Guru"
-  path = "/"
-}
-resource "aws_iam_user" "Test-2" {
-  name = "Prasad"
-  path = "/"
-}
-resource "aws_instance" "ubuntu" {
-  ami = "ami-019715e0d74f695be"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "terraform-instance"
-  }
-}
