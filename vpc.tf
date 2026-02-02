ebbc9a36116714fd51719262d19098a37fd6d126
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2026-02-02 19:21:38 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -54,6 +54,14 @@ resource "aws_route_table" "public_route_table" {
     Name = "public_route_table"
   }
 }
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vz.id
+
+  tags = {
+    Name = "private_route_table"
+  }
+}
+
 # resource "aws_route_table" "private_route_table" {
 #   vpc_id = aws_vpc.example.id
 
