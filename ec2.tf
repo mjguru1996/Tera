bbf55ade642edc5b08a4c9a31a02e6b1e2f65a5a
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2026-02-02 16:48:45 +0530

    Upated

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,6 @@
-resource "aws_iam_user" "Test-1" {
-  name = "Guru"
-  path = "/"
-}
-resource "aws_iam_user" "Test-2" {
-  name = "Prasad"
-  path = "/"
-}
 resource "aws_instance" "ubuntu" {
-  ami = "ami-019715e0d74f695be"
-  instance_type = "t2.micro"
+  ami = var.ami
+  instance_type = var.instance_type
   tags = {
     Name = "terraform-instance"
   }
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+variable "ami" {
+  type = string
+  default = "ami-019715e0d74f695be"
+}
