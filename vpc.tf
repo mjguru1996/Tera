fc32d8e7b3b51282f67ec22a88e3c5bcc8c884a9
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2026-02-02 19:11:36 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -46,13 +46,8 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vz.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw1
-  }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    # egress_only_gateway_id = aws_egress_only_internet_gateway.gw1.id
   }
 
   tags = {
