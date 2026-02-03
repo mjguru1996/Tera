e7911107dd4e357b4820f104948ac0b823bb2ac2
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2026-02-03 07:12:16 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -122,13 +122,13 @@ resource "aws_instance" "dbserver" {
 #     Environment = "Dev"
 #   }
 # }
-terraform {
-  backend "s3" {
-    bucket = "vzdino-tf-state-bucket"
-    key    = "vzdino-tf-state-bucket/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "vzdino-tf-state-bucket"
+#     key    = "vzdino-tf-state-bucket/terraform.tfstate"
+#     region = "ap-south-1"
+#   }
+# }
 output "publicip" {
    value = aws_instance.webserver.id
   
